c15614c7a06e7f85f16b9acd022ab46670b6ee34
Giovanni Alcantara <me@giovannialcantara.com>
2014-11-02 09:46:35 +0000

    Final touches

--- routes/index.js
+++ routes/index.js
@@ -98,9 +98,8 @@ exports.vote = function(socket) {
 	console.log('sess_id during voting: ' + sess_id);
 	socket.on('send:vote', function(data) {
 		var ip;
-		var socket_ = io();
 		socket.on('connect', function(){
-		    ip = socket_.io.engine.id;
+		    ip = socket.socket.io.engine.id;
 		});
 
 		console.log("HEEYYYYY: " + ip);
